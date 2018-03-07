8239ee0e3d99d79b76e70d39967b25769cb150ad
Rajesh kumar <rajeshkumarchelluri@gmail.com>
2018-03-07 20:56:20 +0530

    added categories

---
+++ app/src/main/java/com/quickpick/views/ui/customviews/CustomDialog.java
@@ -0,0 +1,8 @@
+package com.quickpick.views.ui.customviews;
+
+/**
+ * Created by Rajesh kumar on 07-03-2018.
+ */
+
+public class CustomDialog {
+}
---
+++ app/src/main/java/com/quickpick/views/ui/dashboard/GetCategory_Id.java
@@ -0,0 +1,8 @@
+package com.quickpick.views.ui.dashboard;
+
+/**
+ * Created by Rajesh kumar on 07-03-2018.
+ */
+
+public interface GetCategory_Id {
+}
