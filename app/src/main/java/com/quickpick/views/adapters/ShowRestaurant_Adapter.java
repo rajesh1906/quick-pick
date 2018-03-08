6f7adbfd3d273e570e7313a45652d7473c5ec411
rajeshkumarandroid <rajeshk.chelluri@gmail.com>
2018-03-08 15:39:36 +0530

    added some stuff

--- app/src/main/java/com/quickpick/views/adapters/ShowRestaurant_Adapter.java
+++ app/src/main/java/com/quickpick/views/adapters/ShowRestaurant_Adapter.java
@@ -3,9 +3,11 @@ package com.quickpick.views.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,6 +16,7 @@ import com.quickpick.model.restaurant_category.RestaurantData;
 import com.quickpick.views.ui.restarunt.RestaruntActivity;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Rajesh Kumar on 05-09-2017.
@@ -32,10 +35,12 @@ public class ShowRestaurant_Adapter extends RecyclerView.Adapter<ShowRestaurant_
     public class MyViewHolder extends RecyclerView.ViewHolder {
        TextView textView;
        RelativeLayout rel_parent;
+       ImageView img_res;
         public MyViewHolder(View view) {
             super(view);
             textView = (TextView)view.findViewById(R.id.txt_item);
             rel_parent = (RelativeLayout)view.findViewById(R.id.rel_parent);
+            img_res = view.findViewById(R.id.img_res);
         }
     }
     @Override
@@ -49,7 +54,10 @@ public class ShowRestaurant_Adapter extends RecyclerView.Adapter<ShowRestaurant_
     @Override
     public void onBindViewHolder(final MyViewHolder holder,final int position) {
 
+        Log.e("random number is","<<><>"+getRandomNumber());
+
         holder.textView.setText( dataList.get(position).getRes_Name());
+        holder.img_res.setBackgroundResource(getIMage(getRandomNumber()));
         holder.rel_parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,5 +79,39 @@ public class ShowRestaurant_Adapter extends RecyclerView.Adapter<ShowRestaurant_
     public long getItemId(int position) {
         return position;
     }
+
+
+    private int getRandomNumber(){
+        Random r = new Random();
+        int Low = 0;
+        int High = 4;
+        int Result = r.nextInt(High-Low) + Low;
+
+        return Result;
+    }
+
+
+    private int getIMage(int number){
+        int item =0;
+        switch (number){
+            case 0:
+                item = R.drawable.img_one;
+                break;
+            case 1:
+                item = R.drawable.img_two;
+                break;
+            case 2:
+                item = R.drawable.img_three;
+                break;
+            case 3:
+                item = R.drawable.img_four;
+                break;
+        }
+
+        return item;
+    }
+
+
+
 
 }
