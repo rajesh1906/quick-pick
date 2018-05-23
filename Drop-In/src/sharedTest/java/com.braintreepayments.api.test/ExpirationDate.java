8661a44329202cc45150ef0a0d3ddfbc814379b1
rajeshkumarandroid <rajeshk.chelluri@gmail.com>
2018-05-23 18:13:45 +0530

    updated code

--- Drop-In/src/sharedTest/java/com.braintreepayments.api.test/ExpirationDate.java
+++
@@ -1,6 +0,0 @@
-package com.braintreepayments.api.test;
-
-public class ExpirationDate {
-
-    public static final String VALID_EXPIRATION = "1219";
-}
---
+++ app/src/main/java/com/quickpick/views/ui/brain_tree/BrainTreeDashBoard.java
@@ -0,0 +1,100 @@
+package com.quickpick.views.ui.brain_tree;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.braintreepayments.api.dropin.DropInActivity;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+import com.quickpick.R;
+import com.quickpick.views.ui.BaseActivity;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
+
+/**
+ * Created by Rajesh Kumar on 23-05-2018.
+ */
+public class BrainTreeDashBoard extends BaseActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String PATH_TO_SERVER = "PATH_TO_SERVER";
+    private String clientToken;
+    private static final int BRAINTREE_REQUEST_CODE = 4949;
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.brain_tree;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getClientTokenFromServer();
+        Button buyNowButton = (Button)findViewById(R.id.buy_now);
+        buyNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBraintreeSubmit(view);
+            }
+        });
+    }
+
+    public void onBraintreeSubmit(View view){
+        DropInRequest dropInRequest = new DropInRequest().clientToken(clientToken);
+        startActivityForResult(dropInRequest.getIntent(this), BRAINTREE_REQUEST_CODE);
+    }
+
+    private void getClientTokenFromServer(){
+        AsyncHttpClient androidClient = new AsyncHttpClient();
+        androidClient.get(PATH_TO_SERVER, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d(TAG, getString(R.string.token_failed) + responseString);
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseToken) {
+                Log.d(TAG, "Client token: " + responseToken);
+                clientToken = responseToken;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == BRAINTREE_REQUEST_CODE){
+            if (RESULT_OK == resultCode){
+                DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                String paymentNonce = result.getPaymentMethodNonce().getNonce();
+                //send to your server
+                Log.d(TAG, "Testing the app here");
+                sendPaymentNonceToServer(paymentNonce);
+            }else if(resultCode == Activity.RESULT_CANCELED){
+                Log.d(TAG, "User cancelled payment");
+            }else {
+                Exception error = (Exception)data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+                Log.d(TAG, " error exception");
+            }
+        }
+    }
+
+    private void sendPaymentNonceToServer(String paymentNonce){
+        RequestParams params = new RequestParams("NONCE", paymentNonce);
+        AsyncHttpClient androidClient = new AsyncHttpClient();
+        androidClient.post(PATH_TO_SERVER, params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d(TAG, "Error: Failed to create a transaction");
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                Log.d(TAG, "Output " + responseString);
+            }
+        });
+    }
+}
